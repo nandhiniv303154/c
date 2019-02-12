43b83602a33b314641fa933f97075bf688f13428
nandhiniv303154 <45959798+nandhiniv303154@users.noreply.github.com>
2019-02-12 21:40:54 +0530

    Create set12.c

---
+++ set12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[10],ch;
+scanf("%c",&a[10]);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("it's not a vowel");
+}
+}
