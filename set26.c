31dbd9e81630a423bad08044a0cf0c471a214881
nandhiniv303154 <45959798+nandhiniv303154@users.noreply.github.com>
2019-02-27 20:37:50 +0530

    Create set26.c

---
+++ set26.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,j;
+scanf("%d %d",&a,&b);
+for(i=a;i<b;i++)
+{
+int count=0;
+for(j=2;j<i;j++0
+{
+if(i%j==0)
+{
+count=1;
+}
+if(count==0)
+{
+printf("%d\n",i);
+}
+}
+}
+return 0;
+}
