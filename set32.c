65edd9a0466bc487281f98473d0bd335441bab9b
nandhiniv303154 <45959798+nandhiniv303154@users.noreply.github.com>
2019-03-01 16:08:53 +0530

    Create set32.c

---
+++ set32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a[10],max=0;
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<=n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+prrintf("result:%d",max);
+return 0;
+}
