e27f0bbf3998cde7c78fc4b319aed69346706391
nandhiniv303154 <45959798+nandhiniv303154@users.noreply.github.com>
2019-03-25 16:21:16 +0530

    Create set446.c

---
+++ set446.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+char s[50];
+int i,count=0;
+printf("enter the sentence:");
+scanf("%[^\n]s",s);
+for(i=0;s[i]!='\0';i++)
+{
+if(!((s[i]>='0' && s[i]<='9')||(s[i]>='a' && s[i]<='z')))
+{
+count=count+1;
+}
+}
+printf("\nthe number of numeric in the sentence is:%d",count);
+return 0;
+}
